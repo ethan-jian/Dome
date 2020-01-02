4d1b2c57edf431afbd1f7258ff646c193bd3b6d2
ethan <729381317@qq.com>
2020-01-03 01:35:57 +0800

    "test      iiiii"

--- src/main/java/flows/Base.java
+++ src/main/java/flows/Base.java
@@ -10,46 +10,26 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class Base {
+    public static WebDriver driver;
 
-    public WebDriver driver;
+    public List<WebElement> exceElement(String expression) {
 
-    public WebElement[] locateElement(String expression) {
-        /**
-         * 元素xpath定位
-         * @expression传入的xpath表达式
-         * @return一组元素
-         */
-        List<WebElement> elements_list = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(expression)));
-
-        return elements_list.toArray(WebElement[]::new);
-    }
-
-    public void  clickElement(String expression) {
-
-        return this.locateElement(expression).g;
-
-    }
-
-    public void exceElement(String expression, String exceType, String inputText, int waitTime) {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.baidu.com/");
         List<WebElement> webElements = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(expression)));
         WebElement webElement = webElements.get(0);
-        if (exceType.equals("click")){
-            webElement.click();
-        }
 
         for (WebElement e: webElements){
             System.out.println(e.getText());
         }
 
-
+        return webElements;
 
     }
 
     public static void main(String[] args){
         Base base = new Base();
-//        System.out.println(base.exceElement("//*[@id=\"u1\"]//a"));
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.baidu.com/");
+        System.out.println(base.exceElement("//*[@id=\"u1\"]//a"));
     }
 }
