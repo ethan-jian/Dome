b5ec63943f9f34b8e93c0c968f9ad6a03056468e
ethan <729381317@qq.com>
2020-01-03 00:37:19 +0800

    test

---
+++ src/main/java/flows/Base.java
@@ -0,0 +1,55 @@
+package flows;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.lang.model.element.Element;
+import java.util.List;
+
+public class Base {
+
+    public WebDriver driver;
+
+    public WebElement locateElement(String expression) {
+        /**
+         * 元素xpath定位
+         * @expression传入的xpath表达式
+         * @return一组元素
+         */
+        List<WebElement> webElements = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(expression)));
+//        WebElement webElement = webElements.get(0);
+
+        return webElements;
+    }
+
+    public void  clickElement(WebElement element) {
+
+    }
+
+    public void exceElement(String expression, String exceType, String inputText, int waitTime) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.baidu.com/");
+        List<WebElement> webElements = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(expression)));
+        WebElement webElement = webElements.get(0);
+        if (exceType.equals("click")){
+            webElement.click();
+        }
+
+        for (WebElement e: webElements){
+            System.out.println(e.getText());
+        }
+
+
+    }
+
+    public static void main(String[] args){
+        Base base = new Base();
+//        System.out.println(base.exceElement("//*[@id=\"u1\"]//a"));
+    }
+}
