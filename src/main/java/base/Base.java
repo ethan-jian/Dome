fdc8d8b4fbac6d534ea8ae1ac31903285df178cc
ethan <729381317@qq.com>
2020-01-02 14:13:59 +0800

    "test"

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,4 +1,20 @@
 package base;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Base {
+
+    public void exceElement(){
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.baidu.com");
+        WebElement element = (new WebDriverWait(driver,5)).until(ExpectedConditions.presenceOfElementLocated(By.id("kw")));
+
+    }
 }
