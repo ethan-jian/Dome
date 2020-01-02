02a6d8548e19ffc69b6d270ca86e1297cf583279
ethan <729381317@qq.com>
2020-01-02 18:34:58 +0800

    "test"

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ui.Wait;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
