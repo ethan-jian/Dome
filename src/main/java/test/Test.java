a4582be72d2de0fd77f481f0fa3ce81a10daaa1d
ethan <729381317@qq.com>
2020-01-06 22:25:05 +0800

    "test      iiiii"

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,27 +1,25 @@
 package test;
 
-import com.google.inject.internal.cglib.core.$MethodWrapper;
-
-import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
+
+;
 
 public class Test {
     public static void main(String[] args) {
-        List arrayList = new ArrayList();
-        arrayList.add("aaaa");
-        arrayList.add(100);
-
-        for(int i = 0; i< arrayList.size();i++){
-            String item = (String)arrayList.get(i);
-            System.out.println(item);
-//            Log.d("泛型测试","item = " + item);
+        List<Integer> list = List.of(1,2,3,4);
+        Number[] arry = list.toArray(new Number[0]);
+        for (Number n: arry){
+            System.out.println(n);
+        }
+        System.out.println(arry.length);
+        System.out.println(list);
+        System.out.println(arry);
+        Integer[] array = { 1, 2, 3 };
+        System.out.println(Arrays.toString(array));
         }
 
     }
-}
+
 
 
